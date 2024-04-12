845f1b8a8838f10ef0957cda27c739ee326af2f1
hogeyama <gan13027830@gmail.com>
2024-04-13 07:45:32 +0900

    doc: Add a comment

--- app/routes/_auth._lib/index.ts
+++ app/routes/_auth._lib/index.ts
@@ -3,6 +3,9 @@ import invariant from "tiny-invariant";
 
 import { loader } from "../_auth/route";
 
+// 各loader, actionはどうせセッションを確認しないといけないので、
+// これを使う機会はほとんどないかもしれない。
+// （single fetchが有効になったら変わる？）
 export const useUserSession = () => {
   const userSession = useRouteLoaderData<typeof loader>("routes/_auth");
   invariant(userSession, "User session not found");
