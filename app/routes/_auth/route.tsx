806515d745dc0bcbaaeb795f21f419f29e21ba3e
hogeyama <gan13027830@gmail.com>
2024-04-15 22:28:44 +0900

    refactor: Extract NavGroup and NavItem components from NavBar

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -18,6 +18,7 @@ import {
 import clsx, { type ClassValue } from "clsx";
 import type { User } from "lucia";
 import type React from "react";
+import { useCallback } from "react";
 
 import { getSessionOrRedirect } from "~/lib/auth/session.server";
 
@@ -72,40 +73,79 @@ const NavBar: React.FC<{
   classes?: ClassValue[];
 }> = ({ navGroups, classes }) => {
   const { pathname } = useLocation();
-  const style = {
-    container: clsx([
-      ["bg-base-200"],
-      ["shadow-md", "h-screen", "overflow-auto"],
-      ["w-40", "md:w-48", "lg:w-64"],
-      ["flex-none"],
-      classes,
-    ]),
-    menu: clsx("menu", ["menu-sm", "md:menu-md", "lg:menu-lg", "xl:menu-xl"]),
-    menuTitle: clsx(
-      ["menu-title text-base-content"],
-      ["text-sm", "md:text-md", "lg:text-lg"],
-    ),
-    link: (link: string) => (pathname === link ? "active" : ""),
-  };
+  const isItemActive = useCallback(
+    (item: NavItem) => item.link === pathname,
+    [pathname],
+  );
   return (
-    <div className={style.container}>
+    <div
+      className={clsx([
+        ["bg-base-200"],
+        ["shadow-md", "h-screen", "overflow-auto"],
+        ["w-40", "md:w-48", "lg:w-64"],
+        ["flex-none"],
+        classes,
+      ])}
+    >
       {navGroups.map((navGroup, i) => (
-        <div key={navGroup.id}>
-          {i > 0 ? <div className="divider" /> : null}
-          <ul className={style.menu}>
-            <li className={style.menuTitle}>{navGroup.id}</li>
-            {navGroup.items.map((item) => (
-              <li key={item.link}>
-                <Link to={item.link} className={style.link(item.link)}>
-                  <span>{item.icon}</span>
-                  {item.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <NavGroup
+          key={navGroup.id}
+          navGroup={navGroup}
+          pathname={pathname}
+          isItemActive={isItemActive}
+          isFirstChild={i === 0}
+        />
       ))}
     </div>
+  );
+};
+
+const NavGroup: React.FC<{
+  navGroup: NavGroup;
+  pathname: string;
+  isFirstChild: boolean;
+  isItemActive: (item: NavItem) => boolean;
+  classes?: ClassValue[];
+}> = ({ navGroup, isFirstChild, isItemActive, classes }) => {
+  return (
+    <div key={navGroup.id} className={clsx(classes)}>
+      {!isFirstChild ? <div className="divider" /> : null}
+      <ul
+        className={clsx("menu", [
+          "menu-sm",
+          "md:menu-md",
+          "lg:menu-lg",
+          "xl:menu-xl",
+        ])}
+      >
+        <li
+          className={clsx(
+            ["menu-title text-base-content"],
+            ["text-sm", "md:text-md", "lg:text-lg"],
+          )}
+        >
+          {navGroup.id}
+        </li>
+        {navGroup.items.map((item) => (
+          <NavItem key={item.link} item={item} active={isItemActive(item)} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const NavItem: React.FC<{
+  item: NavItem;
+  active: boolean;
+  classes?: ClassValue[];
+}> = ({ item, active, classes }) => {
+  return (
+    <li key={item.link} className={clsx(classes)}>
+      <Link to={item.link} className={active ? "active" : ""}>
+        <span>{item.icon}</span>
+        {item.text}
+      </Link>
+    </li>
   );
 };
 
